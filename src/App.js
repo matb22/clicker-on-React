2589341d2a6fe922ca4a09fc29ecfea1f69dd123
Yuriy <162017185+matb22@users.noreply.github.com>
2025-07-05 00:06:18 +0500

    last changes

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import Header from './components/Header'
+
+import logo from './img/logo.jpg'
+
+
+
+
+
+
+class App extends React.Component {
+    
+
+    constructor (props) { 
+        super(props)
+        this.state={ 
+            count : 0 ,
+        }
+
+        this.onClickPlus = this.onClickPlus.bind(this)
+    }
+    
+    render() {
+        
+        
+        
+        return<>
+
+
+    
+    <div className='main-app'>
+    <Header title ='Счётчик:'/>
+
+    <h1>{this.state.count}</h1>
+    <>
+    <button onClick={this.onClickMinus}>Минус -</button>
+    <button onClick={this.onClickPlus}>Плюс +</button>
+    </>
+    </div>
+        </>
+
+}
+
+    
+    onClickPlus=()=> { 
+        this.setState({count:this.state.count + 1} )    
+        }
+    
+    onClickMinus=()=> {
+        this.setState({count:this.state.count-1})
+    }
+    
+
+}
+
+
+
+
+
+export default App 
---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './css/main.css'
+
+
+
+
+ReactDOM.createRoot(document.getElementById('app')).render(<App></App>)
+
+
